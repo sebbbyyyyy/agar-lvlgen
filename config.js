5fa741dbec761f00987aeee8054983b062859fac
Cr4xy <Cr4xy@Live.de>
2016-03-16 11:32:29 +0100

    Update config.js
    Added new options

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 module.exports = {
 	// Required
-	c_user: "c_user",
-	datr: "datr",
-	xs: "xs",
+	c_user: "100011434068289",
+	datr: "waWgVim4L75xF8mRKbuwypNB",
+	xs: "92%3Aqkn2vpOq3gDqSw%3A2%3A1458040635%3A-1",
 	
-	name: "EATER OF POTATO",
+	name: "agar-lvlgen",
 	
 	// Advanced
 	botLimit: 200,
+	spawnDelay: 100,
+	servers: {ffa: true, teams: false, experimental: true, party: true},
 	regions: ["BR-Brazil", "CN-China", "EU-London", "JP-Tokyo", "RU-Russia", "SG-Singapore", "TK-Turkey", "US-Atlanta"],
 	statusDelay: 1000
 }
